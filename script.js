886a2547df8bd19c81e2c454817d54f111d25c9d
Shubhanshu Rana <72681107+Rana1805@users.noreply.github.com>
2021-10-11 21:22:06 +0530

    Added Clock

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ start.addEventListener("click", ()=>{
         startmenu.style.bottom = "50px"
     }
 })
+
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    //Add a zero in front of numbers<10
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("time").innerHTML = hr + " : " + min + " : " + sec;
+    var time = setTimeout(function(){ startTime() }, 500);
+
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var curWeekDay = days[today.getDay()]
+    document.getElementById("day").innerHTML = curWeekDay;
+
+    var curDay = today.getDate();
+    var curMonth = today.getMonth();
+    var curYear = today.getFullYear();
+    var date = curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
